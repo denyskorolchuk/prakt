c91d97e77d3b799b98a0bcfc4113dde85f111b76
denyskorolchuk <144689385+denyskorolchuk@users.noreply.github.com>
2023-09-27 12:17:45 +0300

    Add files via upload

---
+++ pr2/Program (2).cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        int minPositiveNumber = int.MaxValue;
+
+        Console.WriteLine("Введіть 10 чисел:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Число {i + 1}: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                numbers[i] = number;
+
+              
+                if (number > 0 && number < minPositiveNumber)
+                {
+                    minPositiveNumber = number;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введено некоректне число. Будь ласка, введіть число ще раз.");
+                i--; 
+            }
+        }
+
+        if (minPositiveNumber == int.MaxValue)
+        {
+            Console.WriteLine("Серед введених чисел немає додатніх чисел.");
+        }
+        else
+        {
+            Console.WriteLine($"Найменше додатнє число: {minPositiveNumber}");
+        }
+    }
+}
